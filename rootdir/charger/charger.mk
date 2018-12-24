alrakhesyafauzan/android_device_xiaomi_rolex

rolex/riva: Add MIUI Poweroff Charging Animation
Surprisingly it is working very well while offline charging, same as MIUI

Reference- https://review.lineageos.org/#/c/205385/

Change-Id: I1282a368bc9b0b9cd51cbbcde8d9973dce822de6
Signed-off-by: alrakhesyafauzan <alrakhesyafauzan@gmail.com>

rootdir/charger/charger.mk
@@ -1,0 +1,21 @@
+PRODUCT_COPY_FILES += \
+    device/xiaomi/riva/rootdir/charger/chargeonlymode:$(TARGET_OUT_DIR)/root/sbin/charger \
+    device/xiaomi/riva/rootdir/charger/battery_image_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_0.png \
+    device/xiaomi/riva/rootdir/charger/battery_image_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_1.png \
+    device/xiaomi/riva/rootdir/charger/battery_image_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_2.png \
+    device/xiaomi/riva/rootdir/charger/battery_image_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_3.png \
+    device/xiaomi/riva/rootdir/charger/battery_image_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_4.png \
+    device/xiaomi/riva/rootdir/charger/battery_image_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_5.png \
+    device/xiaomi/riva/rootdir/charger/battery_image_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_6.png \
+    device/xiaomi/riva/rootdir/charger/battery_image_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_7.png \
+    device/xiaomi/riva/rootdir/charger/battery_num_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_0.png \
+    device/xiaomi/riva/rootdir/charger/battery_num_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_1.png \
+    device/xiaomi/riva/rootdir/charger/battery_num_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_2.png \
+    device/xiaomi/riva/rootdir/charger/battery_num_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_3.png \
+    device/xiaomi/riva/rootdir/charger/battery_num_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_4.png \
+    device/xiaomi/riva/rootdir/charger/battery_num_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_5.png \
+    device/xiaomi/riva/rootdir/charger/battery_num_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_6.png \
+    device/xiaomi/riva/rootdir/charger/battery_num_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_7.png \
+    device/xiaomi/riva/rootdir/charger/battery_num_8.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_8.png \
+    device/xiaomi/riva/rootdir/charger/battery_num_9.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_9.png \
+    device/xiaomi/riva/rootdir/charger/battery_num_10.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_10.png

Hyakei/android_device_xiaomi_rolex_old

(cherry picked from commit 6315b26a03abdbf577109f351dd80b82e4eb68ec)
Signed-off-by: Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>

rootdir/charger/charger.mk
@@ -1,0 +1,21 @@
+PRODUCT_COPY_FILES += \
+    device/xiaomi/rolex/rootdir/charger/chargeonlymode:$(TARGET_OUT_DIR)/root/sbin/charger \
+    device/xiaomi/rolex/rootdir/charger/battery_image_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_0.png \
+    device/xiaomi/rolex/rootdir/charger/battery_image_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_1.png \
+    device/xiaomi/rolex/rootdir/charger/battery_image_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_2.png \
+    device/xiaomi/rolex/rootdir/charger/battery_image_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_3.png \
+    device/xiaomi/rolex/rootdir/charger/battery_image_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_4.png \
+    device/xiaomi/rolex/rootdir/charger/battery_image_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_5.png \
+    device/xiaomi/rolex/rootdir/charger/battery_image_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_6.png \
+    device/xiaomi/rolex/rootdir/charger/battery_image_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_7.png \
+    device/xiaomi/rolex/rootdir/charger/battery_num_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_0.png \
+    device/xiaomi/rolex/rootdir/charger/battery_num_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_1.png \
+    device/xiaomi/rolex/rootdir/charger/battery_num_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_2.png \
+    device/xiaomi/rolex/rootdir/charger/battery_num_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_3.png \
+    device/xiaomi/rolex/rootdir/charger/battery_num_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_4.png \
+    device/xiaomi/rolex/rootdir/charger/battery_num_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_5.png \
+    device/xiaomi/rolex/rootdir/charger/battery_num_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_6.png \
+    device/xiaomi/rolex/rootdir/charger/battery_num_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_7.png \
+    device/xiaomi/rolex/rootdir/charger/battery_num_8.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_8.png \
+    device/xiaomi/rolex/rootdir/charger/battery_num_9.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_9.png \
+    device/xiaomi/rolex/rootdir/charger/battery_num_10.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_10.png