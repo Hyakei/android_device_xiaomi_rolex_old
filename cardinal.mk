fc7e07d504a9a19f37b2d8240e2f579743aae8f2
hyakei <oke123543@gmail.com>
2019-05-16 06:26:49 +0700

    cardinal bring up

--- cardinal.mk
+++ cardinal.mk
@@ -20,20 +20,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/custom/common.mk)
+$(call inherit-product, vendor/cardinal/common.mk)
 
-# Maintainer Prop
-PRODUCT_BUILD_PROP_OVERRIDES += \
-DEVICE_MAINTAINERS="Eko"
+# Add Maintainer Prop Inherit from rolex device
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Eko"
 
-# Inherit from rolex device
 $(call inherit-product, device/xiaomi/rolex/device.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 4A
-PRODUCT_NAME := aosp_rolex
+PRODUCT_NAME := cardinal_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo aosp_rolex-userdebug
+add_lunch_combo cardinal_rolex-eng
